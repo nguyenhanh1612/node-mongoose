2fe356b0ed36c99a04b37ddf4b752cf3019fb9a0
nguyenhanh1612 <144312854+nguyenhanh1612@users.noreply.github.com>
2024-09-14 16:23:39 +0700

    Mongoose Part 2

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 const mongoose = require('mongoose');
-const Nations = require('./models/nations');
+const Nations = require('./models/nations'); // Nhập schema Nations
 
-const url = 'mongodb://localhost:27017/football';  // URL kết nối tới MongoDB
+const url = 'mongodb://localhost:27017/football'; // Kết nối với cơ sở dữ liệu MongoDB
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
   console.log('Connected correctly to server');
 
-  var newNation = Nations({
+  // Tạo một quốc gia mới
+  Nations.create({
     name: 'Qatar',
-  });
-
-  // Lưu quốc gia mới
-  newNation.save()
+    description: 'Home Team'
+  })
   .then((nation) => {
     console.log(nation);
-    return Nations.find({}).exec();  // Tìm tất cả các quốc gia
-  })
-  .then((nations) => {
-    console.log(nations);
 
-    return Nations.deleteMany({});  // Xóa tất cả quốc gia
+    // Tìm và cập nhật quốc gia
+    return Nations.findByIdAndUpdate(nation._id, {
+      $set: { description: 'WC 2022' }
+    }, { new: true }).exec();
+  })
+  .then((nation) => {
+    console.log(nation);
+
+    // Thêm một bình luận (sub-document)
+    nation.comments.push({
+      rating: 5,
+      comment: 'Please give me beer!',
+      author: 'Hacker'
+    });
+
+    // Lưu quốc gia đã cập nhật
+    return nation.save();
+  })
+  .then((nation) => {
+    console.log(nation);
+
+    // Xóa quốc gia
+    return Nations.deleteOne({});
   })
   .then(() => {
-    return mongoose.connection.close();  // Đóng kết nối
+    return mongoose.connection.close();
   })
   .catch((err) => {
     console.log(err);
