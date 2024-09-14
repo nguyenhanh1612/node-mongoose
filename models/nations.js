124ca233cc71a586c836720f35f6a6d3cfdb852a
nguyenhanh1612 <144312854+nguyenhanh1612@users.noreply.github.com>
2024-09-14 16:24:52 +0700

    Mongoose Part 3

--- models/nations.js
+++ models/nations.js
@@ -1,16 +1,41 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const nationSchema = new Schema({
-  name: {
+// Định nghĩa schema bình luận
+const commentSchema = new Schema({
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true
+  },
+  comment: {
     type: String,
-    required: true,
-    unique: true
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
   }
 }, {
   timestamps: true
 });
 
-var Nations = mongoose.model('nations', nationSchema);
+// Định nghĩa schema quốc gia
+const nationSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  comments: [commentSchema] // Mảng chứa các bình luận
+}, {
+  timestamps: true
+});
 
-module.exports = Nations;
+var Nations = mongoose.model('Nations', nationSchema); // Tạo model
+module.exports = Nations; // Xuất model
